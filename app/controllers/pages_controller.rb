37417f4c38e477491bd50f3d27996fcdf805a597
1000BPM <66570754+1000BPM@users.noreply.github.com>
2025-02-16 00:07:06 +0000

    Draft 1

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+
+  def home
+
+    @list_of_items = Item.all.order({ :created_at => :desc })
+
+    render({:template => "pages_templates/list"})
+
+  end
+end
